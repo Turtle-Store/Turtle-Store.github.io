7151a0a076fa8e1428d86c358adafd701403d2ca
Turtle-Store <cydiarepository2017@gmail.com>
2016-10-17 18:04:24 -0700

    main

--- update.sh
+++ update.sh
@@ -1,3 +1,8 @@
-rm Packages.bz2
-dpkg-scanpackages -m debs /dev/null >Packages
-bzip2 Packages
+if [[ ! -f Packages.bz2 ]];
+then
+	dpkg-scanpackages -m . /dev/null >Packages
+	bzip2 Packages
+else
+	rm -r *.bz2
+	dpkg-scanpackages -m . /dev/null >Packages && bzip2 Packages
+fi
